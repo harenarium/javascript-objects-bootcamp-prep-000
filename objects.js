3ff714eb9573f08103ee477f9ffe94f29a2c51d3
harenarium <carolinel2610@gmail.com>
2017-11-02 22:25:07 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -4,9 +4,10 @@ var playlist = {
 }
 
 function updatePlaylist (playlist, artistName, songTitle){
-  Object.assign(playlist, {artistName: 'songTitle'})
+  return Object.assign(playlist, {artistName: 'songTitle'})
 }
 
 function removeFromPlaylist(playlist, artistName){
   delete playlist.artistName
+  return playlist
 }
